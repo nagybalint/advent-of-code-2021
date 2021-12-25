001804f45f518a9642a6b30ed22c27c2a0f8514f
Balint Nagy <bnagy@cloudera.com>
2021-12-25 21:19:57 +0100

    Add solution to Day1Task1

--- src/main/scala/nagybalint/tasks/Day1Task1.scala
+++ src/main/scala/nagybalint/tasks/Day1Task1.scala
@@ -2,11 +2,13 @@ package nagybalint.tasks
 
 import nagybalint.Task
 
-import scala.io.Source
-
 class Day1Task1 extends Task {
   override def solve(): Unit = {
-    val ins = inputs.getLines()
-    print(ins)
+    val res = inputs.getLines()
+      .toSeq
+      .map(_.toInt)
+      .sliding(2)
+      .count(l => l.head < l(1))
+    println(res)
   }
 }
